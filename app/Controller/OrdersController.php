874158ec67c97f527cce6fb7012637357c7e5086
unknown <gauravranjan39@gmail.com>
2019-05-03 00:31:57 +0530

    generate random unique order number

--- app/Controller/OrdersController.php
+++ app/Controller/OrdersController.php
@@ -9,13 +9,17 @@ class OrdersController extends AppController {
 		$this->layout = "my_layout";
 		// if (empty($customerId)) {
 		// 	$this->redirect(array('controller'=>'customers','action'=>'index'));
-		// }
+        // }
+        
+
+        die;
 		$this->set('customerId',$customerId);
 		$this->loadModel('Category');
 		$categoryLists = $this->Category->find('list',array('conditions'=>array('Category.parent_id'=>0)));
 		$this->set('categoryLists',$categoryLists);
 		
 		if ($this->request->is('post')) {
+            $orderNumber = 'OD' .$customerId. rand() . time();
 			pr($this->request->data);
 			pr($this->request->data['OrderItem']);die;
 			// foreach ()
